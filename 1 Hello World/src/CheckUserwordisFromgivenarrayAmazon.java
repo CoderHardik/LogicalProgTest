2dcbd99e0eb37a193627b71bc99d8d013e2288a2
CoderHardik <shahhardikv@gmail.com>
2026-02-07 13:33:54 -0800

    Update CheckUserwordisFromgivenarrayAmazon.java

--- 1 Hello World/src/CheckUserwordisFromgivenarrayAmazon.java
+++ 1 Hello World/src/CheckUserwordisFromgivenarrayAmazon.java
@@ -13,7 +13,7 @@ Following program probably does not work if there are duplicate characters like 
 I think proper way to do this would be to create arraylist copy of charcter of original char array and have counter =0;
 	then start loop of string and check each character with arraylist contain method, if character is there then remove that chacter from arraylist first al.remove(user_st(charAt)) and then increment counter if condition met
 	that way next time we do not search same character from that and ultimately if count is 
-/*
+*/
 
 public class CheckUserwordisFromgivenarrayAmazon {
 
@@ -51,7 +51,42 @@ public class CheckUserwordisFromgivenarrayAmazon {
 		
 		s.close();
 		}//end of main
-	
+
+	/*
+Program to tackle 'add' or repeating character problem-
+
+ public static void checkUserWordFromArray(char ch1[], String s){
+        char ch[] = s.toCharArray();
+        ArrayList<Character> al = new ArrayList<>();
+        int count=0;
+        
+        // Create ArraList from Given master Array
+        for (char c:ch1){
+            al.add(c);
+        }
+        //Check if given String is in Arraylist, if so increment count and remove that element from arraylist to avoid detecting same character again
+        for (char c:ch){
+            if (al.contains(c)){
+                count++;
+                al.remove((Character) c);
+            }
+        }
+        // Check if count is same as user string then your string is in master array. Also handle edge case
+        if (ch.length<1){
+            System.out.println("Array is invalid");
+            return;
+        }
+        
+        if (count==ch.length){
+            System.out.println("Your word belongs to master Array");
+        }
+        else
+        {
+            System.out.println("Your word does not belong to master Array");
+        }
+    }
+
+	*/
 		
 	
 
