506cf7abfd76118762e53611b7c90112989b5e0a
CoderHardik <shahhardikv@gmail.com>
2026-02-07 18:24:14 -0800

    Update ClosestPalindromeAmazon.java

--- 1 Hello World/src/ClosestPalindromeAmazon.java
+++ 1 Hello World/src/ClosestPalindromeAmazon.java
@@ -141,6 +141,17 @@ private static long createPalindrome(long prefix, boolean even) {
     String rev = new StringBuilder(res).reverse().toString();
     // If original length was even, reverse the whole prefix. 
     // If odd, drop the last char of prefix before reversing.
-    return Long.parseLong(res + (even ? rev : rev.substring(1)));
+	if (even) {
+    // If length is 4, prefix is "12", reverse is "21". 
+    // We want "12" + "21" = 1221
+    secondHalf = rev;
+} else {
+    // If length is 3, prefix is "12", reverse is "21". 
+    // We skip the '2' and just take '1'.
+    // We want "12" + "1" = 121
+    secondHalf = rev.substring(1);
+}
+
+return Long.parseLong(res + secondHalf);
 }
 */
