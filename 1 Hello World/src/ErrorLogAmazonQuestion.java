721849f02348a518b638e81e3dab2725ddb10303
CoderHardik <shahhardikv@gmail.com>
2026-02-08 20:05:37 -0800

    Update ErrorLogAmazonQuestion.java

--- 1 Hello World/src/ErrorLogAmazonQuestion.java
+++ 1 Hello World/src/ErrorLogAmazonQuestion.java
@@ -65,3 +65,58 @@ And then check which company is affected the most.
 	}//end of method
 
 }//end of class
+
+
+/*
+Better way to program:
+In function;
+1. Create a list of companies
+2. Create a map with company name and their occurance
+3. Find the max occuranc based on collections.max(map.values())
+4. Find the key from for loop based on key that is attached to max values
+
+import java.util.*;
+public class FindMostRecurringCompany {
+    public static void main(String[] args) {
+        // Input as an array of strings (simulating the log file lines)
+        String[] logs = {
+            "Error at time 10:54 is for customer Google",
+            "Error at time 11:00 is for customer Apple",
+            "Error at time 11:05 is for customer Google",
+            "Error at time 11:10 is for customer Apple",
+            "Error at time 11:15 is for customer Apple"
+        };
+
+        analyzeErrors(logs);
+    }
+
+    public static void analyzeErrors(String [] logs){
+        ArrayList <String> CompanyList = new ArrayList<>();
+        //1. Create Arraylist with list of companies from error log
+        for (String comp: logs)
+            { if(comp.startsWith("Error")){
+                String temp [] = comp.split(" ");
+                CompanyList.add(temp[temp.length-1]);
+            }//end of if
+        }//end of for
+        //2. Create map with company and their occurance
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String company: CompanyList){
+            if (hm.containsKey(company)){
+                hm.put(company,hm.get(company)+1);}
+                else{
+                    hm.put(company,1);
+                }  
+            } 
+        //3. Find the maximum occurance of values of company with collections.max    
+        int maxValueInMap=Collections.max(hm.values());
+        //4. Find the name of company based on highest occurance found earlier
+        for (Map.Entry <String, Integer> me: hm.entrySet()){
+            if(me.getValue()==maxValueInMap){
+                System.out.println("highest impacted company is "+me.getKey());
+            }
+        }
+    }//End of analyzeError   
+}
+
+*/
