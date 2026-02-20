c18fa06557fd1be830c31a2c755afa578b0f0ea8
CoderHardik <shahhardikv@gmail.com>
2026-02-20 15:15:29 -0800

    Update MovezeroToEnd.java

--- 1 Hello World/src/MovezeroToEnd.java
+++ 1 Hello World/src/MovezeroToEnd.java
@@ -2,34 +2,31 @@ import java.util.Arrays;
 
 /*Amazon :  Given a number in an array form. Write a program to move all zeros to the end.
  * https://www.techbeamers.com/amazon-quality-assurance-engineer-interview-guide/
+
+ Dont just move and switch zeros, basically it will swap but later when you go to last element which is zero, it will swap again.
  */
 public class MovezeroToEnd {
 
 	public static void main(String[] args) {
 		   int a[] ={1,2,0,4,5};
-		   
-		   System.out.println(Arrays.toString(movezerotoend(a)));
-		    }//end of main
-		    
-		    
-		    public static int[] movezerotoend(int a[]){
+    int res[] = movezero(a);
+    for(int x: res){
+      System.out.println(x);
+    }
+  }
 
-		    	int swap =a.length-1; // Here swap is supposed to be location where we will place out 0
-		    	int count=0; // this is supposed to be loop pointer
-		    	
-		    		
-		    		while(count<swap) {
-		    			if(a[count]==0){
-		    				a[count]=a[swap];
-				    		a[swap]=0;
-				    		swap--;
-		    			}
-		    			count++;
-		    		}
-
-		    	return a;
-
-		    	}
-	    	
+  public static int [] movezero(int A[]){
+    int counter = 0;
+    int temp=0;
+   for (int i = 0; i < A.length; i++){
+      if (A[i]!=0){
+        temp = A[i];
+        A[i]=A[counter];
+        A[counter]=temp;
+        counter++ ;
+      }
+    }
+    return A;
+  }
 	
 }
